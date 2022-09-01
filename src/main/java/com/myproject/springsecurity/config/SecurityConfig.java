44ff51be6b4e8209584bfcbc0b42e6120cdd8439
unknown <bijoy.simple@gmail.com>
2022-09-01 19:17:57 +0530

    security

--- src/main/java/com/myproject/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/myproject/springsecurity/config/SecurityConfig.java
@@ -1,34 +1,18 @@
 package com.myproject.springsecurity.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 //@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter
+public class SecurityConfig 
 {
-    @Override
-    protected void configure(HttpSecurity http) throws Exception
-    {
-    	System.out.println("Security started----");
-        http
-         .csrf().disable()
-         .authorizeRequests().anyRequest().authenticated()
-         .and()
-         .httpBasic();
-    }
-
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth)
-            throws Exception
-    {
-        auth.inMemoryAuthentication()
-        	.withUser("admin")
-        	.password("{noop}password")
-        	.roles("USER");
-    }
+	/*
+	 * @Override protected void configure(HttpSecurity http) throws Exception {
+	 * System.out.println("Security started----"); http .csrf().disable()
+	 * .authorizeRequests().anyRequest().authenticated() .and() .httpBasic(); }
+	 * 
+	 * @Autowired public void configureGlobal(AuthenticationManagerBuilder auth)
+	 * throws Exception { auth.inMemoryAuthentication() .withUser("admin")
+	 * .password("{noop}password") .roles("USER"); }
+	 */
 }
